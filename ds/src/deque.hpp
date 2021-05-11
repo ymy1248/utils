641e1b1e0fb47f54ae13485e6bf4c16dd5b4c18e
ymy1248 <ymy1248@outlook.com>
2021-05-11 22:59:35 +0800

    deque is much harder than I thought

--- ds/src/deque.hpp
+++ ds/src/deque.hpp
@@ -4,18 +4,59 @@
 #include <sys/types.h>
 #include "vector.hpp"
 
+std::deque<int> a;
 namespace ymy {
 
 static const size_t CHUNK_SIZE = 16;
 
+
 template<typename T>
 class deque {
  public:
+  class Iter;
+  class Map;
   void push_back(const T&);
   void push_front(const T&);
+  void pop_front();
+  void pop_back();
  private:
-  vector<T**> map_;
+  Map map_;
+
+ public:
+  class Map {
+   public:
+     void push_back(const T*);
+     void push_front(const T*);
+   private:
+    T** array_;
+  };
+  class Iter {
+   public:
+   private:
+    T* first_;
+    T* last_;
+    T* cur_;
+    T** node_;
+  };
 };
+
+
+template<typename T>
+void
+deque<T>::push_back(const T& val) {
+}
+
+template<typename T>
+void
+deque<T>::push_front(const T& val) {
+}
+
+template<typename T>
+void
+deque<T>::pop_back() {
+}
+
+}
 
 }   // namespace ymy
 
