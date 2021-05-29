f51b3af7c75a4aa2ce25143266f03d78584c030f
ymy1248 <ymy1248@outlook.com>
2021-05-29 22:22:33 +0800

    added some comments

--- cpp/template/traits.cpp
+++ cpp/template/traits.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 
+//! Predefined class for function overloading. These are real traits.
 struct input_iter_tag {};
 struct output_iter_tag {};
 struct forward_iter_tag : public input_iter_tag {};
 struct bid_iter_tag : public forward_iter_tag {};
 struct random_iter_tag : public bid_iter_tag {};
 
-
 class deque {
 public:
   class Iter {
   public:
+    //! Define a unified interface to support Trait's template struct.
+    //! Define the trait of this class.
     using iter_category = random_iter_tag;
   };
 };
@@ -23,11 +25,14 @@ public:
   };
 };
 
+//! Parrots the trait information
 template<typename IterT>
 struct IterTrait{
+  // Effective CPP Item 42: for a nested dependent type name please use typename. Because compiler doesn't know whether this is a static val or global val or a type.
   using iterator_category = typename IterT::iter_category;
 };
 
+//! Using override techniques to do compile time if else. It's like metaprogramming
 template<typename IterT, typename DistT>
 void doAdvance(IterT &iter, DistT d, random_iter_tag) {
   std::cout << "Called random_iter_tag\n";
@@ -38,6 +43,7 @@ void doAdvance(IterT &iter, DistT d, bid_iter_tag) {
   std::cout << "Called bid_iter_tag\n";
 }
 
+//! Wrapper, it makes compiler to decide which function should be used.
 template<typename IterT, typename DistT>
 void advance(IterT &iter, DistT d) {
   doAdvance(iter, d, typename IterTrait<IterT>::iterator_category());
