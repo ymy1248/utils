e4791578dd9e9a0c6b5530c21c2f843f32af4e36
ymy1248 <ymy1248@outlook.com>
2021-09-26 23:49:36 +0800

    thread

--- mt/mutex.cpp
+++ mt/mutex.cpp
@@ -1,6 +1,27 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <shared_mutex>
+#include <thread>
+
 #include "env.hpp"
 
 using namespace std;
 
-int main() {
+std::mutex mtx;
+
+void log(ostringstream *os, char c) {
+    std::lock_guard<std::mutex> lock(mtx);
+    for (int i = 0; i < 10; ++i) {
+        (*os) << c;
+    }
+    (*os) << "\n";
+}
+
+TEST(mutex, basic) {
+    ostringstream os;
+    std::thread t1(log, &os, '1');
+    std::thread t2(log, &os, '2');
+    t1.join();
+    t2.join();
+    EXPECT_EQ(os.str(), string("1111111111\n2222222222\n"));
 }
