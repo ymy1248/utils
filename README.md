b45371e1b8667041e108e4e45d98fe91aa324152
ymy <ymy1248@outlook.com>
2021-11-28 17:55:58 +0800

    fit mac develop env

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 > 
 > --Richard Feynman
 
-After 3 years of working experience, I realized creation is the beast way to learn. This project is also like a C++ reference book for me.
+This project is also like a C++ reference book for me.
 
 ## CPP 
 Directory: [./cpp](./cpp)
@@ -15,6 +15,11 @@ Cpp examples and sandbox mostly from *Effective C++, Effective Modern C++ and C+
 Directory: [./ds](./ds)
 
 Data structure I learned, I won't and can't write it as complex as STL. I can only simply write down the basic idea and algorithm.
+
+## Multi-threading
+Directory: [./mt](./mt)
+
+Test code when reading book *C++ Concurrency in Action*
 
 ## Snippet
 Some snippet code for some util functions.
--- mt/thread.cpp
+++ mt/thread.cpp
@@ -1,19 +1,71 @@
+/**
+ * @file thread.cpp
+ * @author ymy1248 (ymy1248@outlook.com)
+ * @brief Learning std thread.
+ * @version 0.1
+ * @date 2021-11-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <gtest/gtest.h>
 #include <thread>
+#include <mutex>
 #include <iostream>
 
 using namespace std;
 
-int foo(int n) {
-    for (int i = 0; i < n; ++i) {
-        cout << i << endl;
-    }
-    return 0;
+
+TEST(Thread, basic) 
+{
+    class Functor {
+     public:
+        std::mutex &_mtx;
+        void operator()() {
+            _mtx.try_lock();
+            cout << "Do something!" << endl;
+            _mtx.unlock();
+        }
+        explicit Functor(mutex &mtx) : _mtx(mtx) 
+        {}
+    };
+    //! This causes ambiguity, it is like a function declaration.
+    // thread myThread(Functor());
+
+    // Two ways to resolve this problem.
+    // Initialize list.
+    std::mutex mtx;
+    thread myThread1{Functor(mtx)};
+    // Double pair parentheses
+    thread myThread2((Functor(mtx)));
+    myThread1.join();
+    myThread2.join();
 }
 
-int main() {
-    std::thread t(foo, 10);
-    std::thread t1(foo, 10);
-    t.join();
+TEST(Thread, join) 
+{
+    class Functor {
+     public:
+        int &_i;
+        Functor(int &i) : _i(i) {}
+        void operator()()
+        {
+            for (unsigned j = 0; j < 1E8; ++j) {
+                ++_i;
+            }
+        }
+    };
+    int i = 0;
+    thread t1((Functor(i)));
     t1.join();
-    return 0;
+    EXPECT_EQ(t1.joinable(), false);
+    EXPECT_EQ(i, 1E8);
+
+    int i2 = 0;
+    thread t2((Functor(i2)));
+    t2.detach();
+    EXPECT_NE(i2, 1E8);
+    // Sleep for 1 sec to ensure i2 still allocated, otherwise segv.
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
